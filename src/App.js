f479518b8cbbf850a75fcab65ac03c6f18a97301
Jepasq <jepasq111@gmail.com>
2023-02-08 13:49:51 +0100

    Footer uses the footer markup keyword

--- src/App.js
+++ src/App.js
@@ -4,32 +4,34 @@ import './App.css';
 import NetworkStatus from './NetworkStatus';
 
 function ProgramVersion() {
-    return <div className="program-version">TrXplorer v0.0.0-1</div>;
+    return <footer>
+	<div className="program-version">TrXplorer v0.0.0-1</div>
+	</footer>
 }
 
 function App() {
     return (
-	<div>
-	  <div className="App">
-	  <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-          >
-          Learn React
-      </a>
-	  </header>
-	  </div>
+	    <div>
+	    <div className="App">
+	    <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+	className="App-link"
+	href="https://reactjs.org"
+	target="_blank"
+	rel="noopener noreferrer"
+            >
+            Learn React
+	</a>
+	    </header>
+	    </div>
 	    <NetworkStatus />
 	    <ProgramVersion />
 	    </div>
-  );
+    );
 }
 
 export default App;
