7ffde24283bc988730b1ebdd4abc4648a4f4fb47
Jepasq <jepasq111@gmail.com>
2023-05-16 05:59:24 +0200

    Make the CheckAddress widget show result

--- src/CheckAddress.js
+++ src/CheckAddress.js
@@ -15,30 +15,53 @@ class CheckAddress extends React.Component {
 	    isFullnodeConnected: 'null',
 	    isSolidityConnected: 'null',
 	    isEventserverConnected: 'null',
-	    checkResult: 'tested!'
+	    checkResult: 'Waiting...',
+	    address: ''
 	};
 
 	// This binding is necessary to make `this` work in the callback
 	this.checkAddr = this.checkAddr.bind(this);
     }
 
+    changeAddrValue(evt) {
+	const val = evt.target.value;
+	this.setState({
+	    address: val
+	});
+    }
+    
     checkAddr() {
 	const tronweb = new TronWeb(mainOptions);
-	console.log(tronweb.isAddress("aze"));
-	this.setState({
-	    checkResult: "aze"
-	});
+
+	var newSpan;
+	
+	if (tronweb.isAddress(this.state.address)) {
+	    newSpan = (
+		<span>The addresse is <strong>valid</strong>.</span>
+	    )
+	} else {
+	    newSpan = (
+		<span>The addresse is <strong>not valid</strong>.</span>
+	    )
+	}
+	    
+	    this.setState({
+		checkResult: newSpan
+	    });
     }
     
     render() {
 	return <div className="widget">
 		   <div>Check Address :</div>
 		   <div>
-		       <input type="text"></input>
+		       <input type="text"
+			      onChange={evt => this.changeAddrValue(evt)}>
+		       </input>
 		       <button type="button" onClick={this.checkAddr}>
 			   Check
 		       </button>
-		       <span>{this.state.checkResult}</span>
+		       <br />
+		       <div>{this.state.checkResult}</div>
 		   </div>
 	       </div>
     }
